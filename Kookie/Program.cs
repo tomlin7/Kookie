51751a5f942065bf050526167e6f0018d5170621
billy <billydevbusiness@gmail.com>
2021-04-18 13:02:51 +0530

    Parser implementation

--- Kookie/Program.cs
+++ Kookie/Program.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection.Metadata;
+using System.Linq;
 
 namespace Kookie
 {
-    class Program
+    // 1 + 2 * 3
+    // 
+    // 
+    // 
+    //     +
+    //    / \
+    //   1   *
+    //      / \
+    //     2   3
+    
+    // 1 + 2 + 3
+    //
+    //
+    //
+    //       +
+    //      / \
+    //     +   3
+    //    / \
+    //   1   2
+    
+    internal static class Program
     {
         static void Main(string[] args)
         {
@@ -17,21 +37,36 @@ namespace Kookie
                     return;
                 }
 
-                var lexer = new Lexer(line);
-                while (true)
-                {
-                    var token = lexer.NextToken();
-                    if (token.Kind == SyntaxKind.EndOfFileToken)
-                    {
-                        break;
-                    }
-                    Console.Write($"{token.Kind}: '{token.Text}'");
-                    if (token.Value != null)
-                    {
-                        Console.Write($" {token.Value}");
-                    }
-                    Console.WriteLine();
-                }
+                var parser = new Parser(line);
+                var expression = parser.Parse();
+
+                var color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                PrettyPrint(expression);
+                Console.ForegroundColor = color;
+            }
+        }
+
+        static void PrettyPrint(SyntaxNode node, string indent = "")
+        {
+            // ├──
+            // │
+            // └──
+            Console.Write(indent);
+            Console.Write(node.Kind);
+
+            if (node is SyntaxToken token && token.Value != null)
+            {
+                Console.Write(" ");
+                Console.Write(token.Value);
+            }
+            
+            Console.WriteLine();
+            indent += "    ";
+
+            foreach (var child in node.GetChildren())
+            {
+                PrettyPrint(child, indent);
             }
         }
     }
@@ -47,12 +82,19 @@ namespace Kookie
         OpenParenthesisToken,
         CloseParenthesisToken,
         BadToken,
-        EndOfFileToken
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression
     }
     
-    internal class SyntaxToken
+    internal class SyntaxToken : SyntaxNode
     {
-        public SyntaxKind Kind { get; }
+        public override SyntaxKind Kind { get; }
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            return Enumerable.Empty<SyntaxNode>();
+        }
+
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
@@ -160,6 +202,56 @@ namespace Kookie
         }
     }
 
+    internal abstract class SyntaxNode
+    {
+        public abstract SyntaxKind Kind { get; }
+
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+
+    abstract class ExpressionSyntax : SyntaxNode
+    {
+        
+    }
+
+    sealed class NumberExpressionSyntax : ExpressionSyntax
+    {
+        public SyntaxToken NumberToken { get; }
+
+        public NumberExpressionSyntax(SyntaxToken numberToken)
+        {
+            NumberToken = numberToken;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.NumberExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return NumberToken;
+        }
+    }
+
+    sealed class BinaryExpressionSyntax : ExpressionSyntax
+    {
+        public ExpressionSyntax Left { get; }
+        public SyntaxToken OperatorToken { get; }
+        public ExpressionSyntax Right { get; }
+
+        public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxToken operatorToken, ExpressionSyntax right)
+        {
+            Left = left;
+            OperatorToken = operatorToken;
+            Right = right;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return Left;
+            yield return OperatorToken;
+            yield return Right;
+        }
+    }
+    
     internal class Parser
     {
         private readonly SyntaxToken[] _tokens;
@@ -188,6 +280,44 @@ namespace Kookie
         {
             var index = _position + offset;
             return index >= _tokens.Length ? _tokens[^1] : _tokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
+
+        private SyntaxToken NextToken()
+        {
+            var current = Current;
+            _position++;
+            return current;
+        }
+        
+        private SyntaxToken Match(SyntaxKind kind)
+        {
+            if (Current.Kind == kind)
+            {
+                return NextToken();
+            }
+            return new SyntaxToken(kind, Current.Position, null, null);
+        }
+
+        public ExpressionSyntax Parse()
+        {
+            var left = ParsePrimaryExpression();
+
+            while (Current.Kind == SyntaxKind.PlusToken || Current.Kind == SyntaxKind.MinusToken)
+            {
+                var operatorToken = NextToken();
+                var right = ParsePrimaryExpression();
+                left = new BinaryExpressionSyntax(left, operatorToken, right);
+            }
+
+            return left;
+        }
+
+        private ExpressionSyntax ParsePrimaryExpression()
+        {
+            var numberToken = Match(SyntaxKind.NumberToken);
+            return new NumberExpressionSyntax(numberToken);
         }
     }
 }
