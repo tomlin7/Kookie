c2bbc2dbd47b1e4277b19bd28e052ee3cd1f806e
billyateallcookies <billyeatcookies@gmail.com>
2021-05-12 11:46:08 +0200

    Expose SyntaxTree.ParseTokens()

--- Kookie/CodeAnalysis/Syntax/SyntaxTree.cs
+++ Kookie/CodeAnalysis/Syntax/SyntaxTree.cs
@@ -21,5 +21,20 @@ namespace Kookie.CodeAnalysis.Syntax
             var parser = new Parser(text);
             return parser.Parse();
         }
+        
+        public static IEnumerable<SyntaxToken> ParseTokens(string text)
+        {
+            var lexer = new Lexer(text);
+            while (true)
+            {
+                var token = lexer.Lex();
+                if (token.Kind == SyntaxKind.EndOfFileToken)
+                {
+                    break;
+                }
+
+                yield return token;
+            }
+        }
     }
 }
