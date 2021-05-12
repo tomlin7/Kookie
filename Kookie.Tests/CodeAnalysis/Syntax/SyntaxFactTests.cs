94c653acc7823e84706eaaaf62ecf84ecc29b4d5
billyateallcookies <billyeatcookies@gmail.com>
2021-05-12 12:21:06 +0200

    Expose SyntaxFacts.GetText()

---
+++ Kookie.Tests/CodeAnalysis/Syntax/SyntaxFactTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Kookie.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Kookie.Tests.CodeAnalysis.Syntax
+{
+    public class SyntaxFactTests
+    {
+        [Theory]
+        [MemberData(nameof(GetSyntaxKindData))]
+        public void SyntaxFact_GetText_RoundTrips(SyntaxKind kind)
+        {
+            var text = SyntaxFacts.GetText(kind);
+            if (text == null)
+            {
+                return;
+            }
+
+            var tokens = SyntaxTree.ParseTokens(text);
+            var token = Assert.Single(tokens);
+            
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text);
+        }
+
+        public static IEnumerable<object[]> GetSyntaxKindData()
+        {
+            var kinds = (SyntaxKind[]) Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+            {
+                yield return new object[] {kind};
+            }
+        }
+    }
+}
--- Kookie/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ Kookie/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -1,6 +1,6 @@
 ﻿namespace Kookie.CodeAnalysis.Syntax
 {
-    internal static class SyntaxFacts
+    public static class SyntaxFacts
     {
         // -1 * 3
         //
@@ -68,6 +68,43 @@
                 "false" => SyntaxKind.FalseKeyword,
                 _ => SyntaxKind.IdentifierToken
             };
+        }
+
+        public static string GetText(SyntaxKind kind)
+        {
+            switch (kind)
+            {
+                case SyntaxKind.PlusToken:
+                    return "+";
+                case SyntaxKind.MinusToken:
+                    return "-";
+                case SyntaxKind.StarToken:
+                    return "*";
+                case SyntaxKind.SlashToken:
+                    return "/";
+                case SyntaxKind.BangToken:
+                    return "!";
+                case SyntaxKind.EqualsToken:
+                    return "=";
+                case SyntaxKind.AmpersandAmpersandToken:
+                    return "&&";
+                case SyntaxKind.PipePipeToken:
+                    return "||";
+                case SyntaxKind.EqualsEqualsToken:
+                    return "==";
+                case SyntaxKind.BangEqualsToken:
+                    return "!=";
+                case SyntaxKind.OpenParenthesisToken:
+                    return "(";
+                case SyntaxKind.CloseParenthesisToken:
+                    return ")";
+                case SyntaxKind.FalseKeyword:
+                    return "false";
+                case SyntaxKind.TrueKeyword:
+                    return "true";
+                default:
+                    return null;
+            }
         }
     }
 }
