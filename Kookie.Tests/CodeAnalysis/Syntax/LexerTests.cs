f1b8ae0672fcfeb5b28eb92c189b541edefbfec7
billyateallcookies <billyeatcookies@gmail.com>
2021-05-12 11:50:09 +0200

    Rename 'LexerTest' to 'LexerTests'

--- Kookie.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ Kookie.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace Kookie.Tests.CodeAnalysis.Syntax
 {
-    public class LexerTest
+    public class LexerTests
     {
         [Theory]
         [MemberData(nameof(GetTokensData))]
