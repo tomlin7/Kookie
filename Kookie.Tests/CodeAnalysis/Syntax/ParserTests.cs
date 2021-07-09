f7472ff93da842d48092316bb2ef4c4ae3617b3d
billyeatcookies <billydevbusiness@gmail.com>
2021-07-09 02:22:52 -0500

    Add tests to ensure honor unary and binary precedences.

--- Kookie.Tests/CodeAnalysis/Syntax/ParserTests.cs
+++ Kookie.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -74,6 +74,67 @@ namespace Kookie.Tests.CodeAnalysis.Syntax
                 }
             }
         }
+        
+        [Theory]
+        [MemberData(nameof(GetUnaryOperatorPairsData))]
+        public void Parser_UnaryExpression_HonorsPrecedences(SyntaxKind unaryKind, SyntaxKind binaryKind)
+        {
+            var unaryPrecedence = SyntaxFacts.GetUnaryOperatorPrecedence(unaryKind);
+            var binaryPrecedence = SyntaxFacts.GetBinaryOperatorPrecedence(binaryKind);
+            var unaryText = SyntaxFacts.GetText(unaryKind);
+            var binaryText = SyntaxFacts.GetText(binaryKind);
+            var text = $"{unaryText} a {binaryText} b";
+            var expression = SyntaxTree.Parse(text).Root;
+
+            if (unaryPrecedence >= binaryPrecedence)
+            {
+                //   binary
+                //   /   \
+                // unary     b
+                //  |
+                //  a
+
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.UnaryExpression);
+                    
+                    e.AssertToken(unaryKind, unaryText);
+                    
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    
+                    e.AssertToken(binaryKind, binaryText);
+                    
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                }
+            }
+            else
+            {
+                //  unary
+                //    |
+                //  binary
+                //  /   \
+                // a     b
+                
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.UnaryExpression);
+                    e.AssertToken(unaryKind, unaryText);
+                    
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    
+                    e.AssertToken(binaryKind, binaryText);
+                    
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                }
+            }
+        }
 
         public static IEnumerable<object[]> GetBinaryOperatorPairsData()
         {
@@ -82,6 +143,17 @@ namespace Kookie.Tests.CodeAnalysis.Syntax
                 foreach (var op2 in SyntaxFacts.GetBinaryOperatorKinds())
                 {
                     yield return new object[] {op1, op2};
+                }
+            }
+        }
+        
+        public static IEnumerable<object[]> GetUnaryOperatorPairsData()
+        {
+            foreach (var unary in SyntaxFacts.GetUnaryOperatorKinds())
+            {
+                foreach (var binary in SyntaxFacts.GetBinaryOperatorKinds())
+                {
+                    yield return new object[] {unary, binary};
                 }
             }
         }
