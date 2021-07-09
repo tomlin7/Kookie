2931472e32427f18555267ac66d43b6a913e2156
billyeatcookies <billydevbusiness@gmail.com>
2021-07-09 01:59:30 -0500

    Add SyntaxFacts.GetUnaryOperatorKinds() and SyntaxFacts.GetBinaryOperatorKind()

--- Kookie/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ Kookie/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -1,4 +1,7 @@
-﻿namespace Kookie.CodeAnalysis.Syntax
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kookie.CodeAnalysis.Syntax
 {
     public static class SyntaxFacts
     {
@@ -68,6 +71,30 @@
                 "false" => SyntaxKind.FalseKeyword,
                 _ => SyntaxKind.IdentifierToken
             };
+        }
+        
+        public static IEnumerable<SyntaxKind> GetUnaryOperatorKinds()
+        {
+            var kinds = (SyntaxKind[]) Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+            {
+                if (GetUnaryOperatorPrecedence(kind) > 0)
+                {
+                    yield return kind;
+                }
+            }
+        }
+        
+        public static IEnumerable<SyntaxKind> GetBinaryOperatorKinds()
+        {
+            var kinds = (SyntaxKind[]) Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+            {
+                if (GetBinaryOperatorPrecedence(kind) > 0)
+                {
+                    yield return kind;
+                }
+            }
         }
 
         public static string GetText(SyntaxKind kind)
