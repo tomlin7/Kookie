50c564d9a0e67ce13798e3ee477652d256333cc0
billyeatcookies <billydevbusiness@gmail.com>
2021-09-14 07:32:14 -0500

    Fix null reference for cases where the token was inserted

--- Kookie/CodeAnalysis/Syntax/SyntaxToken.cs
+++ Kookie/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -8,7 +8,7 @@ namespace Kookie.CodeAnalysis.Syntax
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
-        public override TextSpan Span => new TextSpan(Position, Text.Length);
+        public override TextSpan Span => new TextSpan(Position, Text?.Length ?? 0);
 
         public SyntaxToken(SyntaxKind kind,int position, string text, object value)
         {
