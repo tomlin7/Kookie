c3025dbdc12a2c918e337cf4f70297d54a103b0f
billyeatcookies <billydevbusiness@gmail.com>
2021-09-14 03:00:23 -0500

    Use `SyntaxFacts.GetText()` in Lexer tests

--- Kookie.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ Kookie.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Kookie.CodeAnalysis.Syntax;
@@ -70,27 +71,21 @@ namespace Kookie.Tests.CodeAnalysis.Syntax
 
         private static IEnumerable<(SyntaxKind kind, string text)> GetTokens()
         {
-            return new[]
+            var fixedTokens = Enum.GetValues(typeof(SyntaxKind))
+                .Cast<SyntaxKind>()
+                .Select(k => (SyntaxKind: k, text: SyntaxFacts.GetText(k)))
+                .Where(t => t.text != null);
+            
+            
+            var dynamicTokens = new[]
             {
-                (SyntaxKind.PlusToken, "+"),
-                (SyntaxKind.MinusToken, "-"),
-                (SyntaxKind.StarToken, "*"),
-                (SyntaxKind.SlashToken, "/"),
-                (SyntaxKind.BangToken, "!"),
-                (SyntaxKind.EqualsToken, "="),
-                (SyntaxKind.AmpersandAmpersandToken, "&&"),
-                (SyntaxKind.PipePipeToken, "||"),
-                (SyntaxKind.EqualsEqualsToken, "=="),
-                (SyntaxKind.BangEqualsToken, "!="),
-                (SyntaxKind.OpenParenthesisToken, "("),
-                (SyntaxKind.CloseParenthesisToken, ")"),
-                (SyntaxKind.FalseKeyword, "false"),
-                (SyntaxKind.TrueKeyword, "true"),
                 (SyntaxKind.NumberToken, "1"),
                 (SyntaxKind.NumberToken, "123"),
                 (SyntaxKind.IdentifierToken, "a"),
                 (SyntaxKind.IdentifierToken, "abc"),
             };
+
+            return fixedTokens.Concat(dynamicTokens);
         }
         
         private static IEnumerable<(SyntaxKind kind, string text)> GetSeparators()
