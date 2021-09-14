b7d527675fb785abb7c697891771cd989680466b
billyeatcookies <billydevbusiness@gmail.com>
2021-09-14 03:47:39 -0500

    Replace if checks with switch expressions in Evaluator

--- Kookie/CodeAnalysis/Evaluator.cs
+++ Kookie/CodeAnalysis/Evaluator.cs
@@ -22,57 +22,64 @@ namespace Kookie.CodeAnalysis
 
         private object EvaluateExpression(BoundExpression node)
         {
-            if (node is BoundLiteralExpression n)
+            return node.Kind switch
             {
-                return n.Value;
-            }
-            
-            if (node is BoundVariableExpression v)
+                BoundNodeKind.LiteralExpression => EvaluateLiteralExpression((BoundLiteralExpression) node),
+                BoundNodeKind.VariableExpression => EvaluateVariableExpression((BoundVariableExpression) node),
+                BoundNodeKind.AssignmentExpression => EvaluateAssignmentExpression((BoundAssignmentExpression) node),
+                BoundNodeKind.UnaryExpression => EvaluateUnaryExpression((BoundUnaryExpression) node),
+                BoundNodeKind.BinaryExpression => EvaluateBinaryExpression((BoundBinaryExpression) node),
+                _ => throw new Exception($"Unexpected node {node.Kind}")
+            };
+        }
+        
+        private object EvaluateLiteralExpression(BoundLiteralExpression boundLiteralExpression)
+        {
+            return boundLiteralExpression.Value;
+        }
+        
+        private object EvaluateVariableExpression(BoundVariableExpression boundVariableExpression)
+        {
+            return _variables[boundVariableExpression.Variable];
+        }
+        
+        private object EvaluateAssignmentExpression(BoundAssignmentExpression boundAssignmentExpression)
+        {
+            var value = EvaluateExpression(boundAssignmentExpression.Expression);
+            _variables[boundAssignmentExpression.Variable] = value;
+            return value;
+        }
+        
+        private object EvaluateUnaryExpression(BoundUnaryExpression boundUnaryExpression)
+        {
+            var operand = EvaluateExpression(boundUnaryExpression.Operand);
+
+            return boundUnaryExpression.Op.Kind switch
             {
-                return _variables[v.Variable];
-            }
-            
-            if (node is BoundAssignmentExpression a)
+                BoundUnaryOperatorKind.Identity => (int) operand,
+                BoundUnaryOperatorKind.Negation => -(int) operand,
+                BoundUnaryOperatorKind.LogicalNegation => !(bool) operand,
+                _ => throw new Exception($"Unexpected unary operator {boundUnaryExpression.Op}")
+            };
+        }
+
+        private object EvaluateBinaryExpression(BoundBinaryExpression boundBinaryExpression)
+        {
+            var left = EvaluateExpression(boundBinaryExpression.Left);
+            var right = EvaluateExpression(boundBinaryExpression.Right);
+
+            return boundBinaryExpression.Op.Kind switch
             {
-                var value = EvaluateExpression(a.Expression);
-                _variables[a.Variable] = value;
-                return value;
-            }
-
-
-            if (node is BoundUnaryExpression u)
-            {
-                var operand = EvaluateExpression(u.Operand);
-
-                return u.Op.Kind switch
-                {
-                    BoundUnaryOperatorKind.Identity => (int) operand,
-                    BoundUnaryOperatorKind.Negation => -(int) operand,
-                    BoundUnaryOperatorKind.LogicalNegation => !(bool) operand,
-                    _ => throw new Exception($"Unexpected unary operator {u.Op}")
-                };
-            }
-
-            if (node is BoundBinaryExpression b)
-            {
-                var left = EvaluateExpression(b.Left);
-                var right = EvaluateExpression(b.Right);
-
-                return b.Op.Kind switch
-                {
-                    BoundBinaryOperatorKind.Addition => (int) left + (int) right,
-                    BoundBinaryOperatorKind.Subtraction => (int) left - (int) right,
-                    BoundBinaryOperatorKind.Multiplication => (int) left * (int) right,
-                    BoundBinaryOperatorKind.Division => (int) left / (int) right,
-                    BoundBinaryOperatorKind.LogicalAnd => (bool) left && (bool) right,
-                    BoundBinaryOperatorKind.LogicalOr => (bool) left || (bool) right,
-                    BoundBinaryOperatorKind.Equals => Equals(left, right),
-                    BoundBinaryOperatorKind.NotEquals => !Equals(left, right),
-                    _ => throw new Exception($"Unexpected binary operator {b.Op}")
-                };
-            }
-            
-            throw new Exception($"Unexpected node {node.Kind}");
+                BoundBinaryOperatorKind.Addition => (int) left + (int) right,
+                BoundBinaryOperatorKind.Subtraction => (int) left - (int) right,
+                BoundBinaryOperatorKind.Multiplication => (int) left * (int) right,
+                BoundBinaryOperatorKind.Division => (int) left / (int) right,
+                BoundBinaryOperatorKind.LogicalAnd => (bool) left && (bool) right,
+                BoundBinaryOperatorKind.LogicalOr => (bool) left || (bool) right,
+                BoundBinaryOperatorKind.Equals => Equals(left, right),
+                BoundBinaryOperatorKind.NotEquals => !Equals(left, right),
+                _ => throw new Exception($"Unexpected binary operator {boundBinaryExpression.Op}")
+            };
         }
     }
 }
